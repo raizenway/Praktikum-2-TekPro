b7d29f5c7f22689eb02ef0e321ba494da1432f6c
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2023-01-31 01:36:54 +0700

    Add files via upload

---
+++ Soal6IO.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/**
+ * <b>Big Number</b><br>
+ * Program untuk menjumlah dan mengalikan
+ * @author Banteng Harisantoso
+ * @version 1.0
+ * @since 30 1 2023
+ */
+
+public class Soal6IO{
+    public static void main(String[] args){
+        //Kamus Data
+        int bilangan1, bilangan2;
+        int jumlah;
+        int kali;
+
+        //Scanner baru
+        Scanner keyboard = new Scanner(System.in);
+
+        //Input
+        bilangan1 = keyboard.nextInt();
+        bilangan2 = keyboard.nextInt();
+        jumlah = bilangan1 + bilangan2;
+        kali = bilangan1 * bilangan2;
+        System.out.println(jumlah);
+        System.out.println(kali);
+
+    }
+}
